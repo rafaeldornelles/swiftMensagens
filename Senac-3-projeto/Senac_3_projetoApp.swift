3513d83fbf0265a6336bc5248dcbcdc143bfeee8
rafael.dornelles <rafafd@hotmail.com>
2021-10-30 09:03:17 -0300

    Initial Commit

---
+++ Senac-3-projeto/Senac_3_projetoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Senac_3_projetoApp.swift
+//  Senac-3-projeto
+//
+//  Created by IOS SENAC on 10/30/21.
+//
+
+import SwiftUI
+
+@main
+struct Senac_3_projetoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
